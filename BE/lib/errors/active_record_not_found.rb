c0e7bfa62ff1bdf184206cb3f7488f831c049cbc
Mai Trong Dung <mai.trong.dung@sun-asterisk.com>
2024-05-17 10:06:07 +0900

    Fix space, indent to 2 and delete folder FE

--- BE/lib/errors/active_record_not_found.rb
+++ BE/lib/errors/active_record_not_found.rb
@@ -1,45 +1,45 @@
 module Errors
-    class ActiveRecordNotFound < Errors::ApplicationError
-      attr_reader :model, :field, :detail, :message_key
+  class ActiveRecordNotFound < Errors::ApplicationError
+    attr_reader :model, :field, :detail, :message_key
   
-      def initialize error, message: nil
-        @model = error.model.underscore
-        @detail = error.class.to_s.split("::")[1].underscore
-        @field = error.primary_key
-        @message_key = message || :default
-        @errors = serialize
+    def initialize error, message: nil
+      @model = error.model.underscore
+      @detail = error.class.to_s.split("::")[1].underscore
+      @field = error.primary_key
+      @message_key = message || :default
+      @errors = serialize
+    end
+  
+    def serialize
+      [
+        {
+          resource: resource,
+          field: field,
+          code: code,
+          message: message
+        }
+      ]
       end
   
-      def serialize
-        [
-          {
-            resource: resource,
-            field: field,
-            code: code,
-            message: message
-          }
-        ]
-      end
-  
-      private
-      def message
+    private
+    def message
         I18n.t message_key,
           scope: [:api, :errors, :messages, :not_found],
           resource: resource
-      end
+    end
   
-      def resource
+    def resource
         I18n.t model,
           locale: :api,
           scope: [:api, :errors, :resources],
           default: model
-      end
+    end
   
-      def code
-        I18n.t detail,
+    def code
+      I18n.t detail,
           locale: :api,
           scope: [:api, :errors, :code],
           default: detail
-      end
+    end
     end
 end  
