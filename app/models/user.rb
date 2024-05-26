1e6d14c95c52b3264992e7abe08c0a82440d31c7
Mai Trong Dung <mai.trong.dung@sun-asterisk.com>
2024-05-26 23:13:50 +0900

    [BE] Validate model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@
 
 class User < ApplicationRecord
   has_one_attached :avatar
+
+  enum :original_role, {Engineer: 0, BrSE: 1}
+  enum :training_role, {BE: 0, FE: 1, Mobile: 2}
+  enum :role, {admin: 0, user: 1}
+
+  validates :name_roma, length: {maximum: Settings.user.max_length.name_roma}
+  validates :name_kata, length: {maximum: Settings.user.max_length.name_kata}
+  validates :email, length: {maximum: Settings.user.max_length.email},
+                    presence: true, uniqueness: true,
+                    format: {with: Settings.user.regex_email}
+  validates :address, length: {maximum: Settings.user.max_length.address}
 end
