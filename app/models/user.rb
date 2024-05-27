6aff51f06b8cb33b8ab54129aeef21ed41f4e5c4
Mai Trong Dung <mai.trong.dung@sun-asterisk.com>
2024-05-27 18:59:25 +0900

    [BE] Fix regex email user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@
 class User < ApplicationRecord
   has_one_attached :avatar
 
-  enum :original_role, {Engineer: 0, BrSE: 1}
-  enum :training_role, {BE: 0, FE: 1, Mobile: 2}
+  enum :original_role, {engineer: 0, brse: 1}
+  enum :training_role, {be: 0, fe: 1, mobile: 2}
   enum :role, {admin: 0, user: 1}
 
   validates :name_roma, length: {maximum: Settings.user.max_length.name_roma}
